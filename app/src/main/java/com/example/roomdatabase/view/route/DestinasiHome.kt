81b6d537bb38f77eddb8dda0f237a7d548dc8b2a
Sulthonhanan <hananakhnaf31@gmail.com>
2025-11-27 17:37:39 +0700

    perubahan Destinasi home dan entry

--- app/src/main/java/com/example/roomdatabase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiHome.kt
@@ -1,6 +1,8 @@
 package com.example.roomdatabase.view.route
 
 import com.example.roomdatabase.R
-object DestinasiEntry : DestinasiNavigasi {
-    override val route = "item_entry"
-    override val titleRes = R.string.entry_siswa }
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes: Int = R.string.judul_home
+}
