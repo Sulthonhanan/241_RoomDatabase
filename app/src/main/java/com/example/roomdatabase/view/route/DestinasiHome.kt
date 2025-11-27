34862e0f01e15d8ec0ad71ef2dea3311cedb58fe
Sulthonhanan <hananakhnaf31@gmail.com>
2025-11-27 16:54:42 +0700

    penambahan destinasi home

---
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,7 @@
+package com.example.roomdatabase.view.route
+
+import com.example.roomdatabase.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa }
