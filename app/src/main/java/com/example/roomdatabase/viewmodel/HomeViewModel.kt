53e152415a84e531435154d61ec2c8b075acc75f
Sulthonhanan <hananakhnaf31@gmail.com>
2025-11-27 16:54:26 +0700

    penambahan halaman home view model

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/HomeViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.roomdatabase.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.repositori.RepositoriSiswa
+import com.example.roomdatabase.room.Siswa
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+private val HomeViewModel.viewModelScope: CoroutineScope
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+        )
+
+}
+
+data class HomeUiState(
+    val listSiswa: List<Siswa> = listOf()
+)
